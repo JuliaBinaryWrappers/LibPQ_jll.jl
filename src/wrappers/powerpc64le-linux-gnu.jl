066109ea8ee7d77c91c6bffba43bc49b20273a64
jlbuild <juliabuildbot@gmail.com>
2022-04-15 17:12:55 +0000

    LibPQ_jll build 14.1.0+1

--- src/wrappers/powerpc64le-linux-gnu.jl
+++ src/wrappers/powerpc64le-linux-gnu.jl
@@ -2,10 +2,11 @@
 export LIBPQ_HANDLE
 
 using OpenSSL_jll
+using Kerberos_krb5_jll
 JLLWrappers.@generate_wrapper_header("LibPQ")
 JLLWrappers.@declare_library_product(LIBPQ_HANDLE, "libpq.so.5")
 function __init__()
-    JLLWrappers.@generate_init_header(OpenSSL_jll)
+    JLLWrappers.@generate_init_header(OpenSSL_jll, Kerberos_krb5_jll)
     JLLWrappers.@init_library_product(
         LIBPQ_HANDLE,
         "lib/libpq.so",
